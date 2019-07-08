c561f72cb6518b5cecb60c092ae3dcf4579253b3
DermotBoyle <dermotboyled@gmail.com>
2019-07-08 11:01:49 +0200

    task 030

--- front/src/signup.js
+++ front/src/signup.js
@@ -13,7 +13,14 @@ class SignUp extends Component {
     return (
       <div>
         <h1>{this.state.value}</h1>
-        <input onChange={this.updateEmailField} type="email" name="email" />
+        <form>
+          <input onChange={this.updateEmailField} type="email" name="email" />
+          <input type="Name" />
+          <input type="LastName" />
+          <input type="password" />
+          <input type="passwordCheck" />
+          <input type="submit" />
+        </form>
       </div>
     );
   }
